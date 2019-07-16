8069e9cce0460e73783e71f8e09f5b6b1aaaab08
deepak <seleniumoar1234@gmail.com>
2019-07-16 07:46:35 +0530

    sadf

---
+++ MavenSeleniumProject/src/test/java/com/acttime/customerTest/CustomerTest.java
@@ -0,0 +1,32 @@
+package com.acttime.customerTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+	
+	@Test(groups= {"smokeTest"})
+	public void createCustomerTest() {
+		System.out.println("execute  createCustomerTest");
+	}
+	
+	
+	@Test
+	public void modifyCustomerTest() {
+		System.out.println("execute  createCustomerTest");
+	}
+	
+	
+	@Test
+	public void searchCustomerTest() {
+		System.out.println("search  createCustomerTest");
+	}
+	
+	@Test
+	public void deleteCustomerTest() {
+		System.out.println("search  deleteCustomerTest");
+
+	}
+
+}
